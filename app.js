2fd9ea1c74dc29d0b5a6ef0a9f4257e4de0edb8f
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-05-14 15:48:57 -0400

    completed all api exercises

--- app.js
+++ app.js
@@ -7,22 +7,96 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('./public'));
 
-const todoList = [
+let todoList = [
   {
     id: 1,
     todo: 'Implement a REST API',
   },
+  {
+    id: 2,
+    todo: 'Get Yoked',
+  },
+  {
+    id: 3,
+    todo: 'Eat Protein',
+  },
+  {
+    id: 4,
+    todo: 'Sleep Enough',
+  },
+  {
+    id: 5,
+    todo: 'Wrestle Rottweilers',
+  },
 ];
 
 // GET /api/todos
+// app.get("/api/todo", (req, res) => res.send(todoList.filter(todoItems => todoItems.todo;)));
+app.get("/api/todo", (req, res) => {
+  return res.json(todoList);
+});
 
 // GET /api/todos/:id
+app.get("/api/todo/:id", (req, res) => res.send(todoList.filter(todoItems => {
+  // console.log(todoItems.id, req.params.id);
+  return todoItems.id == req.params.id;
+})));
 
 // POST /api/todos
+app.post("/api/todo", (req, res) => {
+  if (req.body.todo) {
+    const maxId = todoList.reduce((max, currentTodo) => {
+      if (currentTodo.id > max) {
+        max = currentTodo.id
+      }
+      return max
+    }, 0);
 
-// PUT /api/todos/:id
+    const newTodo = {
+      id: maxId + 1,
+      todo: req.body.todo,
+    };
+    todoList.push(newTodo);
+    res.json(req.body);
+  } else {
+    res.status(400).json({
+      error: 'Please provide todo text',
+    })
+  }
+});
 
+// PUT /api/todos/:id can t find 404 if found update
+
+app.put("/api/todo/:id", (req, res) => {
+  let newTodo = null;
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id == req.params.id) {
+      newTodo = { id: req.params.id, todo: req.body.todo };
+      todoList.splice(i, 1, newTodo);
+      res.json(newTodo);
+    }
+  }
+  if (newTodo === null) {
+    res.status(404).json({
+      error: "Page Not Found",
+    });
+  }
+})
 // DELETE /api/todos/:id
+app.delete("/api/todo/:id", (req, res) => {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id == req.params.id) {
+      todoList.splice(i, 1);
+      res.json(todoList);
+    }
+  }
+  if (req.params === null) {
+    res.status(404).json({
+      error: "Page Not Found",
+    });
+  }
+})
+
 
 app.listen(3000, function () {
   console.log('Todo List API is now listening on port 3000...');
